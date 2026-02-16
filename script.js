ad49446ead8043ab965f43e7db92e390d0c882c6
Atharva <subhashramteke7@gmail.com>
2026-02-16 22:47:37 +0530

    Connected the frontend with the backend

--- script.js
+++ script.js
@@ -2,27 +2,53 @@ const addTaskButton = document.getElementById("AddTaskButton");
 const taskList = document.getElementById("TaskList");
 
 addTaskButton.addEventListener("click", function () {
-    const title = document.getElementById("title").value.trim();
-    const description = document.getElementById("description").value.trim();
+    const titleInput = document.getElementById("title");
+    const descriptionInput = document.getElementById("description");
+
+    const title = titleInput.value.trim();
+    const description = descriptionInput.value.trim();
 
     if (title === "" || description === "") {
-        alert("Please fill in both title and description!");
+        alert("Please fill in both title and description");
         return;
     }
 
-    const taskItem = document.createElement("li");
 
-    taskItem.innerHTML = `
-    <strong>${title}</strong><br>
-    <span>${description}</span>
-    <br>
-    <button class="deleteBtn">Delete</button>
-    `;
+    fetch("http://127.0.0.1:5000/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            description: description
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to create task");
+        }
+        return response.json();
+    })
+    .then(data => {
 
-    taskList.appendChild(taskItem);
+        const taskItem = document.createElement("li");
 
-    document.getElementById("title").value = "";
-    document.getElementById("description").value = "";
+        taskItem.innerHTML = `
+            <strong>${data.title}</strong><br>
+            <span>${data.description}</span><br>
+            <button class="deleteBtn">Delete</button>
+        `;
+
+        taskList.appendChild(taskItem);
+
+        titleInput.value = "";
+        descriptionInput.value = "";
+    })
+    .catch(error => {
+        console.error(error);
+        alert("Something went wrong");
+    });
 });
 
 taskList.addEventListener("click", function (event) {
